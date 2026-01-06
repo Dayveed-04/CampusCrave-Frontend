aede7122828db73c8139a472a7c54c3699213fe7
Olusanya Taire <olusanyataire@gmail.com>
2026-01-06 14:16:10 +0100

    I did the first 4 pages of the pwa and I also added a public folder with a sub folder for image storage

--- src/app/auth/page.js
+++ src/app/auth/page.js
@@ -2,9 +2,44 @@
 
 export default function PreAuth() {
   return (
-    <div>
-      Pre Auth  Page
+    <div className="min-h-screen flex flex-col items-center px-8 py-6 font-sans">
+
+      {/* Header */}
+      <div className="mt-2">
+        <h4 className="font-bold text-black text-base">
+          Campus Crave
+        </h4>
+      </div>
+
+      {/* Image */}
+      <div className="mt-8 w-52 h-52 rounded-full overflow-hidden">
+        <img 
+          src="/images/food.jpg" 
+          alt="Food" 
+          className="w-full h-full object-cover"
+        />
+      </div>
+
+      {/* Text Content */}
+      <div className="mt-6 text-center">
+        <h2 className="text-2xl font-bold text-black leading-tight">
+          All your <br /> favorite foods
+        </h2>
+        <p className="mt-2 text-xs text-black opacity-60 leading-relaxed">
+          Order your favorite menu with easy <br />on-demand delivery
+        </p>
+      </div>
+
+      {/* Buttons */}
+      <div className="mt-12 mb-6 w-full max-w-sm space-y-3">
+        <button className="w-full py-3.5 bg-black text-white rounded-2xl font-medium hover:bg-gray-900 transition duration-300">
+          Sign In
+        </button>
+        <button className="w-full py-3.5 bg-black text-white rounded-2xl font-medium hover:bg-gray-900 transition duration-300">
+          Register
+        </button>
+      </div>
       
     </div>
   );
-}
+} 
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,15 +1,30 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Pacifico } from 'next/font/google';
+import "./globals.css";
+import { Inter } from 'next/font/google';
+import "./globals.css";
 
-
+{/*const inter = Inter({
+  subsets: ['latin'],
+  variable: '--font-rounded',
+})*/}
+const pacifico = Pacifico(
+{
+  weight: '400',
+  subsets: ['latin'],
+  variable: '--font-logo',
+}
+)
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-      >
+      <body className={pacifico.variable}>
         {children}
       </body>
     </html>
-  );
+  )
 }
+
+
