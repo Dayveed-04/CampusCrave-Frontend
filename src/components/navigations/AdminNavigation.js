c59fd05a9ae6e1553fd5b113d0063ca3008dc41c
Olusanya Taire <olusanyataire@gmail.com>
2026-02-16 21:52:39 +0100

    I edited the vendor menu page and did the bottom nav of the admin page. I also added an Icon

---
+++ src/components/navigations/AdminNavigation.js
@@ -0,0 +1,63 @@
+'use client';
+
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import { images } from '@/constants/image';
+
+const AdminNavigation = () => {
+  const pathname = usePathname();
+
+  const navItems = [
+    {
+      name: 'Home',
+      href: '/vendor/dashboard',
+      icon: images.icons.HomeIcon,
+    },
+    {
+      name: 'Vetting',
+      href: '/vendor/orders',
+      icon: images.icons.OrderIcon,
+    },
+    {
+      name: 'Profile',
+      href: '/vendor/profile',
+      icon: images.icons.ProfileIcon,
+    },
+  ];
+
+  return (
+    <nav className="fixed bottom-0 left-0 right-0 bg-[#EDE7B5]  border-t border-black pb-safe z-50">
+      <div className="flex items-center justify-around h-16 max-w-lg mx-auto">
+        {navItems.map((item) => {
+          const isActive = pathname === item.href;
+          const Icon = item.icon;
+          
+          return (
+            <Link
+              key={item.name}
+              href={item.href}
+              className={`flex flex-col items-center justify-center flex-1 h-full space-y-1 ${
+                isActive 
+                  ? 'text-black' 
+                  : 'text-gray-600'
+              }`}
+            >
+              <img 
+                src={item.icon} 
+                alt={`${item.name} icon`} 
+                className={`w-6 h-6 ${isActive ? 'filter brightness-0 saturate-100' : 'opacity-60'}`} 
+              />
+
+              <span className="text-xs font-medium">{item.name}</span>
+              {isActive && (
+                <div className="absolute bottom-0 w-12 h-1 bg-black rounded-t-full" />
+              )}
+            </Link>
+          );
+        })}
+      </div>
+    </nav>
+  );
+};
+
+export default AdminNavigation;
